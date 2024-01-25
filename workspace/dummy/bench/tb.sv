6189a28ed5d7ed519e3ad143e1ccff6046b2a086
Gabriel Villanova Novaes Magalhães <gabriel.magalhaes@virtus.ufcg.edu.br>
2024-01-25 13:34:01 -0300

    feat: creates a dummy tb

---
+++ workspace/dummy/bench/tb.sv
@@ -0,0 +1,16 @@
+/**
+ * testbench template
+ *
+ * @version: 0.1
+ * @author : Gabriel Villanova N. M.
+ */
+
+ module tb;
+
+  // main block
+  initial begin
+    $display("\n\nWhat do wanna test?\n");
+    $finish();
+  end
+
+endmodule
